780d4aadee65fc63c25a339d33f64ad920a5b117
eunice_sin_h <71563392+EuniceHG@users.noreply.github.com>
2023-07-28 10:01:35 +0000

    Finale

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 //import react into the bundle
 import React from 'react';
-import { createRoot } from 'react-dom';
+import { createRoot } from "react-dom/client";
 
 //include your index.scss file into the bundle
 import "../styles/index.css";
